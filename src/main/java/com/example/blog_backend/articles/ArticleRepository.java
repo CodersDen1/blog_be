d47ef7a5fe891b425ad24fcdf53cc4c5dfc17e0f
CodersDen1 <com.github.adbhut@gmail.com>
2024-01-04 22:43:30 +0530

    first commit

---
+++ src/main/java/com/example/blog_backend/articles/ArticleRepository.java
@@ -0,0 +1,2 @@
+package com.example.blog_backend.articles;public interface ArticleRepository {
+}
---
+++ src/main/java/com/example/blog_backend/comments/CommentEntity.java
@@ -0,0 +1,41 @@
+package com.example.blog_backend.comments;
+
+import com.example.blog_backend.articles.ArticleEntity;
+import com.example.blog_backend.users.UserEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.lang.Nullable;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@NoArgsConstructor(access =  AccessLevel.PROTECTED)
+public class CommentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(nullable = false)
+    private Long id;
+
+    @Nullable
+    private String title;
+
+    @NonNull
+    private String body;
+
+    @CreatedDate
+    private Date createdAt;
+
+
+    @ManyToOne
+    @JoinColumn(name = "articleId", nullable = false)
+    private ArticleEntity article;
+
+    @ManyToOne
+    @JoinColumn(name = "authorid" , nullable = false)
+    private UserEntity user;
+}
