c66622dc0475dc8f4f554b595a9cefbc1ce1aa85
CodersDen1 <com.github.adbhut@gmail.com>
2024-01-05 10:24:28 +0530

    SecurityFilterChain implemented - BASIC

---
+++ src/main/java/com/example/blog_backend/security/AppSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.blog_backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.BasicPermission;
+import java.security.Security;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorizeHttpRequest) -> authorizeHttpRequest
+                        .requestMatchers(HttpMethod.POST, "/users", "/users/login").permitAll().anyRequest().authenticated()).formLogin(Customizer.withDefaults());
+
+
+        return http.build();
+
+    }
+
+}
--- src/main/java/com/example/blog_backend/users/UserController.java
+++ src/main/java/com/example/blog_backend/users/UserController.java
@@ -38,6 +38,10 @@ public class UserController {
     }
 
 
+    @GetMapping("")
+    public String hello(){
+        return "hello world api";
+    }
 
 
 
